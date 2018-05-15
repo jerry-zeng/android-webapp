748f1903b73f5402c597c9116dd6b05a0fdb8058
Jerry <zlfeng_2012@163.com>
2018-05-15 15:08:18 +0800

    -增加单元测试Debug类

---
+++ app/src/test/java/com/jerry/android/blogapp/Debug.java
@@ -0,0 +1,9 @@
+package com.jerry.android.blogapp;
+
+public class Debug
+{
+    public static void Log(String msg)
+    {
+        System.out.println( "[UnitTest] " + msg);
+    }
+}
--- app/src/test/java/com/jerry/android/blogapp/ExampleUnitTest.java
+++ app/src/test/java/com/jerry/android/blogapp/ExampleUnitTest.java
@@ -10,8 +10,16 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
     @Test
     public void addition_isCorrect() {
+        Debug.Log("addition_isCorrect()");
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void another_test(){
+        Debug.Log("another_test()");
+        assertEquals(3, 2 + 1);
     }
 }
